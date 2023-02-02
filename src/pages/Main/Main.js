b1d3a669589af0f84929087bf295d3023e7aefac
Jangchan0 <jchy16@gmail.com>
2023-02-02 13:34:50 +0900

    ADD:  limitBox 컴포넌트 제작

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Slide from './Component/Slide';
 import MiniSlider from './Component/MiniSlider';
+import LimitBox from './Component/LimitBox';
 
 const Main = () => {
   return (
     <div>
       <Slide />
       <MiniSlider />
+      <LimitBox />
     </div>
   );
 };
