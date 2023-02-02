484683b2dd247a5f3b8a4aed376f946a8ba7ede8
Jangchan0 <jchy16@gmail.com>
2023-02-02 13:34:50 +0900

    ADD: 메인 미니 슬라이드 제작

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Slide from '../ListPage/Slide';
+import MiniSlider from '../ListPage/MiniSlider';
+
 const Main = () => {
   return (
     <div>
       <Slide />
+      <MiniSlider />
     </div>
   );
 };
