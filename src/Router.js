0e37636ba6a2ffc5913ea54cf0cf0f8f22bda16f
Hailey_ys <whassup_man@naver.com>
2023-02-08 20:16:54 +0900

    Modify main conflict

--- src/Router.js
+++ src/Router.js
@@ -13,12 +13,7 @@ import MyPage from './pages/MyPage/MyPage';
 import UserInfo from './pages/MyPage/components/UserInfo';
 import Review from './pages/MyPage/components/Review';
 import Refunds from './pages/MyPage/components/Refunds';
-<<<<<<< HEAD
-import Subscribe from './pages/MyPage/components/Subscribe';
-import MovieCart from './pages/MovieCart/MovieCart';
-=======
 import Detail from './pages/MovieDetail/Detail';
->>>>>>> 052d24a (충돌수정)
 
 export default function Router() {
   return (
@@ -30,20 +25,12 @@ export default function Router() {
         <Route path="/login" element={<Login />} />
         <Route path="/category" element={<Category />} />
         <Route path="/auth/kakao/callback" element={<KakaoLogin />} />
-<<<<<<< HEAD
-        <Route path="/subscription" element={<Subscription />} />
-=======
-        <Route path="/moviecart" element={<MovieCart />} />
         <Route path="/subscribe" element={<Subscription />} />
         <Route path="/search" element={<Search />} />
         <Route path="/detail/:id" element={<Detail />} />
-        {/* <Route path="/approval" element={<PaySuccess />} /> */}
->>>>>>> 052d24a (충돌수정)
         <Route path="/mypage" element={<MyPage />}>
-          <Route path="subscribe" element={<Subscribe />} />
           <Route path="reviews" element={<Review />} />
           <Route path="userinfo" element={<UserInfo />} />
-          <Route path="moviecart" element={<MovieCart />} />
           <Route path="refunds" element={<Refunds />} />
         </Route>
       </Routes>
