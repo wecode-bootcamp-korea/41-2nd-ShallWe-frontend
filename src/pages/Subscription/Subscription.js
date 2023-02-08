b285416defb818a4805ee667607d376fc6d133c9
jakesungjaekim <codejakecarry@gmail.com>
2023-02-08 18:48:57 +0900

    Add: Subscription 페이지 수정

--- src/pages/Subscription/Subscription.js
+++ src/pages/Subscription/Subscription.js
@@ -1,152 +1,57 @@
 import React from 'react';
 import styled from 'styled-components';
-import Card from '../../components/Card';
-import Button from '../../components/Button';
+import SubCard from './components/SubCard';
+
+const CARD_LISTS = [
+  {
+    title: '쉘위티켓',
+    price: '20,000',
+    img: 'subimg1',
+    intro: '다양한 독립영화 구경하러가기',
+    day: '매월 1일 결제!',
+    rewards: [
+      {
+        reward: '매주영화티켓1장무료',
+      },
+      {
+        reward: '매달신작영화굿즈',
+      },
+    ],
+  },
+  {
+    title: '쉘위박스',
+    price: '25,000',
+    img: 'subimg2',
+    intro: '다양한 독립영화 구경하러가기',
+    day: '매월 1일 결제!',
+    rewards: [
+      {
+        reward: '매주영화티켓1장무료',
+      },
+      {
+        reward: '매달신작영화굿즈',
+      },
+      {
+        reward: '신작 영화 ! 시사회 초대',
+      },
+      {
+        reward: '관람료상시 할인20%',
+      },
+    ],
+  },
+];
 
 const Subscription = () => {
   return (
     <SubscriptionContainer>
-      <ItemCard className="mr-30">
-        <Card className="mt-20" width="363px" height="405px" border="none">
-          <CardImg src={require('./images/subimg1.png')} alt="subimg1" />
-          <TextCard>
-            <div className="header">
-              <span className="bold">담화박스</span>
-              <span className="blue">39,000원 / 월</span>
-            </div>
-            <div className="description">다양한 독립영화 구경하러가기</div>
-            <div className="divider"></div>
-            <div className="date-wrapper">
-              <div className="date">결제일</div>
-              <div>02월 13일</div>
-            </div>
-            <div className="date-wrapper">
-              <div className="date">도착일</div>
-              <div>02월 16일</div>
-            </div>
-            <Button
-              className="button"
-              width="75px"
-              height="28px"
-              bgColor="rgb(222, 230, 239)"
-              color="#0097F3"
-              fontSize="11px">
-              힌트 공개
-            </Button>
-          </TextCard>
-        </Card>
-      </ItemCard>
-      <ItemCard>
-        <Card className="mt-20" width="363px" height="405px" border="none">
-          <CardImg src={require('./images/subimg2.png')} alt="subimg2" />
-          <TextCard>
-            <div className="header">
-              <span className="bold">담화박스</span>
-              <span className="blue">39,000원 / 월</span>
-            </div>
-            <div className="description">다양한 독립영화 구경하러가기</div>
-            <div className="divider"></div>
-            <div className="date-wrapper">
-              <div className="date">결제일</div>
-              <div>02월 13일</div>
-            </div>
-            <div className="date-wrapper">
-              <div className="date">도착일</div>
-              <div>02월 16일</div>
-            </div>
-            <Button
-              className="button"
-              width="75px"
-              height="28px"
-              bgColor="rgb(222, 230, 239)"
-              color="#0097F3"
-              fontSize="11px">
-              힌트 공개
-            </Button>
-          </TextCard>
-        </Card>
-      </ItemCard>
+      {CARD_LISTS.map((card, idx) => {
+        return <SubCard card={card} key={idx} data={CARD_LISTS} />;
+      })}
     </SubscriptionContainer>
   );
 };
 
 export default Subscription;
-
-const TextCard = styled.div`
-  position: absolute;
-  bottom: 38.94px;
-  left: 14px;
-
-  width: 335px;
-  height: 208px;
-  padding: 20px;
-  border-radius: 10px;
-  background-color: white;
-
-  line-height: 1.5715;
-
-  .header {
-    display: flex;
-    align-items: center;
-
-    .bold {
-      font-size: 20px;
-      font-weight: bold;
-      margin-right: 20px;
-      color: #707070;
-    }
-    .blue {
-      font-size: 15px;
-      font-weight: bold;
-      color: #0097f3;
-    }
-  }
-
-  .description {
-    margin-top: 8px;
-    font-size: 16px;
-    color: #707070;
-  }
-
-  .divider {
-    width: 20px;
-    height: 1px;
-    border: 1px solid rgb(224, 224, 224);
-    margin-top: 10px;
-    margin-bottom: 10px;
-  }
-
-  .date-wrapper {
-    display: flex;
-
-    font-size: 14px;
-    color: rgb(112, 112, 112);
-    .date {
-      margin-right: 5px;
-    }
-  }
-  .button {
-    margin-top: 10px;
-    border: none;
-  }
-`;
-
-const CardImg = styled.img`
-  width: 100%;
-  height: 100%;
-  border-radius: 10px;
-`;
-
-const ItemCard = styled.div`
-  position: relative;
-  width: 363px;
-  height: 609px;
-  cursor: pointer;
-
-  .mt-20 {
-    margin-top: 20px;
-  }
-`;
 
 const SubscriptionContainer = styled.div`
   width: 100%;
---
+++ src/pages/Subscription/components/SubCard.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import styled from 'styled-components';
+import Card from '../../../components/Card';
+import Button from '../../../components/Button';
+import { useNavigate } from 'react-router-dom';
+
+const SubCard = ({ card }) => {
+  const navigate = useNavigate();
+
+  return (
+    <ItemCard className="mr-30">
+      <Card className="mt-20" width="363px" height="405px" border="none">
+        <CardImg src={require(`../images/${card.img}.png`)} alt="subimg1" />
+        <TextCard>
+          <div className="header">
+            <span className="bold">{card.title}</span>
+            <span className="blue">
+              {card.price}원 / {card.day}
+            </span>
+          </div>
+          <div
+            className="description"
+            onClick={() => {
+              navigate('/category');
+            }}>
+            {card.intro} {'>'}
+          </div>
+          <div className="divider"></div>
+          <div className="date-wrapper">
+            {card['rewards'].map((reward) => {
+              return <span>{reward.reward}</span>;
+            })}
+          </div>
+          <Button
+            className="button"
+            width="75px"
+            height="28px"
+            bgColor="rgb(222, 230, 239)"
+            color="#0097F3"
+            fontSize="11px">
+            후원하기
+          </Button>
+        </TextCard>
+      </Card>
+    </ItemCard>
+  );
+};
+
+export default SubCard;
+
+const TextCard = styled.div`
+  position: absolute;
+  bottom: 38.94px;
+  left: 14px;
+
+  width: 335px;
+  height: 208px;
+  padding: 20px;
+  border-radius: 10px;
+  background-color: white;
+
+  line-height: 1.5715;
+
+  .header {
+    display: flex;
+    align-items: center;
+
+    .bold {
+      font-size: 20px;
+      font-weight: bold;
+      margin-right: 20px;
+      color: #707070;
+    }
+    .blue {
+      font-size: 15px;
+      font-weight: bold;
+      color: #0097f3;
+    }
+  }
+
+  .description {
+    margin-top: 8px;
+    font-size: 16px;
+    color: #707070;
+    text-decoration: none;
+  }
+
+  .divider {
+    width: 20px;
+    height: 1px;
+    border: 1px solid rgb(224, 224, 224);
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+
+  .date-wrapper {
+    display: flex;
+    justify-content: space-around;
+    font-size: 11px;
+    color: rgb(112, 112, 112);
+
+    span {
+      width: 20%;
+      height: 40px;
+      overflow-wrap: break-word;
+
+      &:hover {
+        color: orange;
+      }
+    }
+  }
+  .button {
+    margin-top: 10px;
+    border: none;
+  }
+`;
+
+const CardImg = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+`;
+
+const ItemCard = styled.div`
+  position: relative;
+  width: 363px;
+  height: 609px;
+  cursor: pointer;
+
+  .mt-20 {
+    margin-top: 20px;
+  }
+`;
